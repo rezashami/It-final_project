000b6066de043a56134d4ba2ff57b53a17e7902f
rezashami <reza.shami.tanha@gmail.com>
2019-08-31 14:25:46 +0430

    report show fixed and menu added to this activity

--- app/src/main/java/com/example/rezatanha/finalproject/View/report/ReportShowActivity.java
+++ app/src/main/java/com/example/rezatanha/finalproject/View/report/ReportShowActivity.java
@@ -2,13 +2,13 @@ package com.example.rezatanha.finalproject.View.report;
 
 import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.widget.Button;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.example.rezatanha.finalproject.Model.Report.Report;
@@ -39,56 +39,71 @@ public class ReportShowActivity extends AppCompatActivity {
         Bundle b = this.getIntent().getExtras();
         if (b != null) {
             report = (Report) b.getSerializable("Report");
-        }
-        txt_header.setText(report.getHeader());
-        txt_body.setText(report.getBody());
-        PersianDate pdate = new PersianDate(report.getDate().getTime());
-        txt_date.setText(pdate.toString());
-        Button removeButton = findViewById(R.id.btn_remove_report);
-        removeButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                AlertDialog.Builder alert = new AlertDialog.Builder(ReportShowActivity.this);
-                alert.setTitle("حذف گزارش");
-                alert.setMessage("آیا مطمئن هستید؟");
-                alert.setPositiveButton("بلی", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                        Intent replyIntent = new Intent();
-                        replyIntent.putExtra("Removed item", report);
-                        setResult(REPORT_DELETE_RESULT_CODE, replyIntent);
-                        finish();
-                    }
-                });
-                alert.setNegativeButton("خیر", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                        // close dialog
-                        dialog.cancel();
-                    }
-                });
-                alert.show();
-            }
-        });
-        Button shareButton = findViewById(R.id.btn_share_report);
-        shareButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String text = "سربرگ: " + report.getHeader() + "\nبدنه: " + report.getBody() + "\nتاریخ: " + report.getDate();
-                Intent sendIntent = new Intent();
-                sendIntent.setAction(Intent.ACTION_SEND);
-                sendIntent.putExtra(Intent.EXTRA_TEXT, text);
-                sendIntent.setType("text/plain");
-                startActivity(sendIntent);
-            }
-        });
+            if (report != null) {
+                txt_header.setText(report.getHeader());
+                txt_body.setText(report.getBody());
+                PersianDate persianDate = new PersianDate(report.getDate().getTime());
+                txt_date.setText(persianDate.toString());
+            } else
+                finish();
 
+        } else
+            finish();
     }
+
     @Override
     protected void attachBaseContext(Context newBase) {
         super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
     }
+
     @Override
     public boolean onSupportNavigateUp() {
         onBackPressed();
         return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.show_report_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.show_report_share_report:
+                share();
+                return true;
+            case R.id.show_report_delete_report:
+                delete();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+
+    private void delete() {
+        AlertDialog.Builder alert = new AlertDialog.Builder(ReportShowActivity.this);
+        alert.setTitle("حذف گزارش");
+        alert.setMessage("آیا مطمئن هستید؟");
+        alert.setPositiveButton("بلی", (dialog, which) -> {
+            Intent replyIntent = new Intent();
+            replyIntent.putExtra("Removed item", report);
+            setResult(REPORT_DELETE_RESULT_CODE, replyIntent);
+            finish();
+        });
+        alert.setNegativeButton("خیر", (dialog, which) -> dialog.cancel());
+        alert.show();
+    }
+
+    private void share() {
+        String text = "سربرگ: " + report.getHeader() + "\nبدنه: " + report.getBody() + "\nتاریخ: " + report.getDate();
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, text);
+        sendIntent.setType("text/plain");
+        startActivity(sendIntent);
     }
 }
